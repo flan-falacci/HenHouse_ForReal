11ce70400e74bf938387a6cd0b465ffda6d20f26
Flan <mfalacci@gmail.com>
2023-06-15 11:48:08 -0400

    scene checking system

---
+++ Assets/Scipts/sceneChecker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChecker : MonoBehaviour
+{
+    public static int sceneCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+       sceneCount += 1;
+
+        Debug.Log("scenes witnessed: " + sceneCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
