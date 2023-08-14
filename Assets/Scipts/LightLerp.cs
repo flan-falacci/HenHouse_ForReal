d3f5ac08327db1a5b507ef2733c8ca012947b57c
Flan <mfalacci@gmail.com>
2023-08-14 16:38:42 -0400

    monday work1

---
+++ Assets/Scipts/LightLerp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightLerp : MonoBehaviour
+{
+    Vector3 startAngle;
+    Vector3 endAngle;
+    float t;
+    public float riseRate;
+
+    Light myLight;
+
+    void Start()
+    {
+        startAngle = new Vector3(-152.8f,-30,-1.5f);
+        endAngle = new Vector3(-19.7f,-30,0);
+
+        myLight = GetComponent<Light>();
+
+        transform.eulerAngles = startAngle;
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime * riseRate;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(endAngle), t);
+    }
+}
