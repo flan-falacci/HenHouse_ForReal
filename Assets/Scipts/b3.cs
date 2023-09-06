852ba6438da568863d176cc209b2e6728219d321
Flan <mfalacci@gmail.com>
2023-09-06 17:25:29 -0400

    connected tv scene and worked on others

---
+++ Assets/Scipts/b3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class b3 : MonoBehaviour
+{
+    public float timer;
+    float limit; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        limit = 82; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; 
+
+        if (timer >= limit)
+        {
+            SceneManager.LoadScene("ending");
+        }
+    }
+}
