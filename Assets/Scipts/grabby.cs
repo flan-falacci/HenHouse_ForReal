2ac57d0af5b11c39de713c35029bdf94acd72184
Flan <mfalacci@gmail.com>
2023-09-11 11:34:04 -0400

    craft scene

---
+++ Assets/Scipts/grabby.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class grabby : MonoBehaviour
+{
+    public Image hand; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hand.transform.position = Input.mousePosition;
+    }
+}
