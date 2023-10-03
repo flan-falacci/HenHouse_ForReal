7f566b87951cb1447c64014aafc59947e2b41780
Flan <mfalacci@gmail.com>
2023-10-03 16:47:32 -0400

    restart button message that fades out

---
+++ Assets/Scipts/fadeOut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class fadeOut : MonoBehaviour
+{
+    float timer;
+    float t;
+
+    public float fadeSpeed; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        t = Time.deltaTime * fadeSpeed; 
+  
+        Text thisText = gameObject.GetComponent<Text>(); 
+
+        if (timer >= 3)
+        {
+            thisText.color = Color.Lerp(thisText.color, new Color(thisText.color.r, thisText.color.g, 
+                thisText.color.b, 0), t);
+        }
+    }
+}
